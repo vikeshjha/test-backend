dac884a766dff07c91f8f554990546a1b293b68e
vikesh jha <vikeshjha39@gmail.com>
2025-07-23 16:30:19 +0530

    hee

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(cors({
 
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URL)
+mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log('MongoDB connected'))
 .catch(err => console.log(err));
 
